8dc9801e5b48b27dc34f770d68d8c3d55a731095
ashokkumar-nandurka_mars <ashokkumar.nandurka@effem.com>
2025-11-07 22:18:34 +0530

    login security check

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,8 +1,36 @@
-import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Outlet, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { addUser } from "../utils/userSlice";
 import NavBar from "./NavBar";
 import Footer from "./Footer";
 
 const Body = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const userData = useSelector((store) => store.user);
+
+  const fetchUser = async () => {
+    if (userData && userData._id) return;
+    try {
+      const response = await axios.get(`${BASE_URL}/profile/view`, {
+        withCredentials: true,
+      });
+      dispatch(addUser(response.data));
+    } catch (error) {
+      if (error.status === 401) {
+        navigate("/login");
+      }
+      console.error("Error fetching user data:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
   return (
     <div>
       <NavBar />
