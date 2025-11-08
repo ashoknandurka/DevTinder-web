1c65863af61b1f96aba86e6c1aaf2e41e0f25437
ashokkumar-nandurka_mars <ashokkumar.nandurka@effem.com>
2025-11-08 16:09:01 +0530

    feed api and store added

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,5 +1,37 @@
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addFeed } from "../utils/feedSlice";
+import UserCard from "./UserCard";
+
 const Feed = () => {
-  return <div>Feed</div>;
+  const dispatch = useDispatch();
+  const feed = useSelector((store) => store.feed);
+
+  const fetchFeed = async () => {
+    if (feed) return;
+    try {
+      const res = await axios.get(`${BASE_URL}/feed`, {
+        withCredentials: true,
+      });
+      dispatch(addFeed(res.data.data));
+    } catch (error) {
+      console.error("Failed to fetch feed:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchFeed();
+  }, []);
+
+  return (
+    feed && (
+      <div className="flex justify-center my-10">
+        <UserCard user={feed[0]} />
+      </div>
+    )
+  );
 };
 
 export default Feed;
